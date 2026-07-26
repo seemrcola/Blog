9e4d8d3aaef989624eadf8c43d0b86ebc342f51a
seemr <110106669+seemrcola@users.noreply.github.com>
2026-07-27 00:58:55 +0800

    Simplify blog layout and code styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ export default defineConfig({
   output: 'static',
   site: 'https://seemrcola.github.io',
   base: '/Blog',
+  markdown: {
+    shikiConfig: {
+      theme: 'vitesse-light',
+    },
+  },
 });
